180ff5662040426ae1a8b22641c10fee8ae7b6c0
ChenYutongTHU <chenytjudy@gmail.com>
2024-01-27 09:22:42 +0100

    metric

--- metrics.py
+++ metrics.py
@@ -22,7 +22,10 @@ from tqdm import tqdm
 from utils.image_utils import psnr
 from argparse import ArgumentParser
 from glob import glob 
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
 
+'''
 def readImages(renders_dir, gt_dir):
     renders = []
     gts = []
@@ -35,6 +38,32 @@ def readImages(renders_dir, gt_dir):
         gts.append(tf.to_tensor(gt).unsqueeze(0)[:, :3, :, :].cuda())
         image_names.append(fname)
     return renders, gts, image_names
+''' 
+
+class ImageDataset(Dataset):
+    def __init__(self, renders_dir, gt_dir):
+        self.renders_dir = renders_dir
+        self.gt_dir = gt_dir
+        self.image_names = os.listdir(renders_dir)
+
+    def __len__(self):
+        return len(self.image_names)
+
+    def __getitem__(self, idx):
+        fname = self.image_names[idx]
+
+        render_path = os.path.join(self.renders_dir, fname)
+        gt_path = os.path.join(self.gt_dir, fname)
+
+        render = Image.open(render_path)
+        gt = Image.open(gt_path)
+
+        render =    tf.to_tensor(render).unsqueeze(0)[:, :3, :, :]
+        gt = tf.to_tensor(gt).unsqueeze(0)[:, :3, :, :]
+
+        return render, gt, fname
+    
+
 
 def evaluate(model_path, scale):
 
@@ -67,16 +96,21 @@ def evaluate(model_path, scale):
             if not os.path.isdir(renders_dir):
                 renders_dir = method_dir / "test_preds_-1"
 
-            renders, gts, image_names = readImages(renders_dir, gt_dir)
+            #renders, gts, image_names = readImages(renders_dir, gt_dir)
 
             ssims = []
             psnrs = []
             lpipss = []
+            image_names = []
 
-            for idx in tqdm(range(len(renders)), desc="Metric evaluation progress"):
-                ssims.append(ssim(renders[idx], gts[idx]))
-                psnrs.append(psnr(renders[idx], gts[idx]))
-                lpipss.append(lpips_fn(renders[idx], gts[idx]).detach())
+            #for idx in tqdm(range(len(renders)), desc="Metric evaluation progress"):
+            for render, gt, name in tqdm(ImageDataset(renders_dir, gt_dir), desc="Metric evaluation progress"):
+                render = render.cuda()
+                gt = gt.cuda()
+                ssims.append(ssim(render, gt))
+                psnrs.append(psnr(render, gt))
+                lpipss.append(lpips_fn(render, gt).detach())
+                image_names.append(name)
 
             print("  SSIM : {:>12.7f}".format(torch.tensor(ssims).mean(), ".5"))
             print("  PSNR : {:>12.7f}".format(torch.tensor(psnrs).mean(), ".5"))
